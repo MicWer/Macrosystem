03c6a84e173621f9d411074314a787e21fca79a2
Michał Wereszczyński <michal.wereszczynski@willson-brown.com>
2017-12-11 11:14:33 +0100

    Created repository

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    entry: "./js/main.jsx",
+    output: { filename: "./app/app.js" },
+    watch: true,
+    module: {
+        loaders: [
+            {
+                test: /\.jsx$/, exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: { presets: ['es2015', 'stage-2', 'react'] }
+            },
+            {
+                test: /\.css$/,
+                loaders: ['css-loader', 'style-loader']
+            }
+        ]
+    }
+};
